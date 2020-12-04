b64ca1282256a94a0d8f4a0f5d272cf3d08388e4
Maarten Suetens <maarten@intigriti.com>
2020-12-04 21:02:36 +0100

    day 1 done

---
+++ 1/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+// readLines reads a whole file into memory
+// and returns a slice of its lines.
+func readLines(path string) ([]int, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []int
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		num, _ := strconv.Atoi(scanner.Text())
+		lines = append(lines, num)
+	}
+	return lines, scanner.Err()
+}
+
+func compute(lines []int, target int) int {
+	for i := 0; i < len(lines); i++ {
+		for j := i + 1; j < len(lines); j++ {
+			for k := j + 1; k < len(lines); k++ {
+				if lines[j]+lines[i]+lines[k] == target {
+					return lines[j] * lines[i] * lines[k]
+				}
+			}
+		}
+	}
+	return 0
+}
+
+func main() {
+	target := 2020
+	lines, err := readLines("input")
+	if err != nil {
+		log.Fatalf("readLines: %s", err)
+	}
+	for i, line := range lines {
+		fmt.Println(i, line)
+	}
+
+	fmt.Println(compute(lines, target))
+}
